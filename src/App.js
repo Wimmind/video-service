df65fc29ffbb24d2a6d37dc4a2c2e5b1393fc396
Wimmind <terehov.anton@bk.ru>
2020-07-26 22:43:14 +0400

    feat: add autorization functional

--- src/App.js
+++ src/App.js
@@ -9,8 +9,14 @@ import IndexMovies from './components/index-movies/index-movies';
 import IndexChannels from './components/index-channels/index-channels';
 export default class App extends Component {
   state = {
-   isMovies : true
+    isMovies: true,
+    modalHidden: true,
+    userName: localStorage.getItem('userName') || '',
+    userNameForHeader: localStorage.getItem('userName') || '',
+    isUserNameEnter : false
   }
+
+  
 
   onMovie = () => {
     this.setState({isMovies: true})  
@@ -20,12 +26,49 @@ export default class App extends Component {
     this.setState({isMovies: false})  
   }
 
+  closeModalBlock = (e) => {
+    e.preventDefault()
+    this.setState({modalHidden: true}) 
+    this.setState({userNameForHeader: this.state.userName})  
+    this.setState({isUserNameEnter:false}) 
+    localStorage.setItem('userName', this.state.userName);
+  }
+
+  hiddenUsername = () => {
+    this.setState({isUserNameEnter: true})
+  }
+
+  showModalBlock = (e) => {
+    e.preventDefault()
+    this.setState({modalHidden: false})  
+  }
+
+  logout = (e) => {
+    e.preventDefault();
+    this.setState({userName: ''})  
+    this.setState({userNameForHeader:''}) 
+  
+  }
+
+  changeUserName = (e) => {
+    e.preventDefault();
+    this.setState({userName: e.target.value})  
+  }
+
 
   render () {
-    const {isMovies} = this.state;
+    const {
+      isMovies,
+      modalHidden,
+      userName,
+      userNameForHeader,
+      isUserNameEnter
+    } = this.state;
 
     let tagMovies = 'tab-item';
     let tagChannels = 'tab-item';
+    let modalBlock = 'authorization-block';
+    let fadeBlock = 'fade-block';
     let index;
 
     if (isMovies){
@@ -37,9 +80,23 @@ export default class App extends Component {
       index = <IndexChannels/>
     }
 
+    if (modalHidden){
+      modalBlock += ' hidden';
+      fadeBlock += ' hidden'
+    }
+
     return (
       <div className='app-wrapper'>
-       <Header/>
+       <Header 
+        buttonModalFunc={this.showModalBlock}
+        logoutButton = {this.logout}
+        userName={userNameForHeader}
+        userNameChange ={userName}
+        inputFuncChange = {this.changeUserName}
+        inputFuncBlur = {this.closeModalBlock}
+        isUserNameEnter = {isUserNameEnter}
+        hiddenUsername = {this.hiddenUsername}
+      />
        <main className="main">
         <div className={base.wrapper}>
           <div className="tabs-container">
@@ -52,6 +109,39 @@ export default class App extends Component {
         </div>
        </main>
        <Footer/>
+       <div className={fadeBlock}>
+         <form className={modalBlock}>
+           <p className="authorization-block_title">Вход</p>
+          <input 
+            className='input-inlet'
+            placeholder="Логин" 
+            autoFocus={true}
+            onChange={this.changeUserName}
+            value={userName}
+          />
+          <input 
+            className='input-inlet'
+            placeholder="Пароль" 
+            
+          />
+          <div className='checkbox-group'>
+          <input
+            className='input-checkbox'
+            type='checkbox'
+            id='checkbox'
+          />
+          <label htmlFor='checkbox'>
+            Запомнить
+          </label>
+          </div>
+          <button 
+            onClick={this.closeModalBlock}
+            className="modal-sign-in-button" 
+            type="submit"
+          >Войти
+          </button> 
+         </form>
+        </div>
       </div>
     );
   }
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,46 +6,93 @@ import logo from '../../assets/icon/logo.png';
 
 
 export default class Header extends Component {
-  state = {
-    userName: ''
-  }
+    state = {
+        
+    }
 
-  render () {
-    const {userName} = this.state;
 
-    return (
-        <header className="header">
-            <div className={base.wrapper + ' ' +"header-wrapper"}>
-                <a 
-                    className="header-logo"
-                    href={'./'}
-                    >
-                    <img 
-                        src={logo}
-                        width={182}
-                        height={36}
-                        alt="Видеосервис"
-                    />
-                </a>
-                <form className="search-group">
-                    <input 
-                        className="search-input" 
-                        placeholder="Поиск..." 
-                        autoFocus=""
+    render () {
+        const {
+            buttonModalFunc,
+            logoutButton,
+            userName,
+            inputFuncChange,
+            inputFuncBlur,
+            hiddenUsername,
+            isUserNameEnter,
+            userNameChange
+        } = this.props;
+
+        let enterButton;
+        let enterButtonText;
+        let enterButtonFunction;
+
+        let inputUserName;
+        let userNameText;
+        if (isUserNameEnter) {
+            userNameText = 'hidden'
+            inputUserName = 'username-input'
+        } else {
+            userNameText = 'username-text'
+            inputUserName = 'username-input hidden'
+        }
+
+
+        if (userName.length) {
+            enterButton = 'sign-out-button'
+            enterButtonText = 'Выйти'
+            enterButtonFunction = logoutButton;
+        } else {
+            enterButton = 'sign-in-button'
+            enterButtonText = 'Войти'
+            enterButtonFunction = buttonModalFunc;
+        }
+        return (
+            <header className="header">
+                <div className={base.wrapper + ' ' +"header-wrapper"}>
+                    <a 
+                        className="header-logo"
+                        href={'./'}
+                        >
+                        <img 
+                            src={logo}
+                            width={182}
+                            height={36}
+                            alt="Видеосервис"
+                        />
+                    </a>
+                    <form className="search-group">
+                        <input 
+                            className="input-inlet search-input" 
+                            placeholder="Поиск..." 
+                            ref={el=> { this.el = el; }}
+                        />
+                        <button 
+                            className="search-button"
+                            type="submit"
+                        >Найти
+                        </button>
+                    </form>
+                    <div className='username-group'>
+                    <span 
+                        onClick={hiddenUsername}
+                        className={userNameText}
+                    >{userName}</span>
+                    <input
+                        className={inputUserName}
+                        onChange = {inputFuncChange}
+                        onBlur = {inputFuncBlur}
+                        value = {userNameChange}
                     />
                     <button 
-                        className="search-button"
+                        onClick={enterButtonFunction}
+                        className={enterButton} 
                         type="submit"
-                    >Найти
-                    </button>
-                </form> 
-                <button 
-                    className="sign-in-button" 
-                    type="submit"
-                >Войти
-                </button> 
-            </div>
-        </header>
-    );
-  }
+                    >{enterButtonText}
+                    </button> 
+                    </div>
+                </div>
+            </header>
+        );
+    }
 }
